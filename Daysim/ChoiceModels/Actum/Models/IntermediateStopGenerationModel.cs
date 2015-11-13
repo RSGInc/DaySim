1ef6d551f91287a16b583b57ee01527b74eaf56a
JohnLBPhD <John_L_Bowman@alum.mit.edu>
2015-11-13 15:05:52 -0500

    Added a comment line only

--- Daysim/ChoiceModels/Actum/Models/IntermediateStopGenerationModel.cs
+++ Daysim/ChoiceModels/Actum/Models/IntermediateStopGenerationModel.cs
@@ -4,6 +4,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // 
+//
 // Copyright 2011-2013 John Bowman, Mark Bradley, and RSG, Inc.
 // 
 // This file is part of Daysim.
