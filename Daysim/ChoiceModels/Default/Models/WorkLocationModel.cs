ed37ac0f9d66416d4e6d285bc341dd4a60bcffa9
mabcal <mark.bradley@rsginc.com>
2015-11-13 15:47:39 -0800

    Close #16

--- Daysim/ChoiceModels/Default/Models/WorkLocationModel.cs
+++ Daysim/ChoiceModels/Default/Models/WorkLocationModel.cs
@@ -303,7 +303,10 @@ namespace Daysim.ChoiceModels.Default.Models {
 
 				// set shadow price depending on persontype and add it to utility
 				// we are using the sampling adjustment factor assuming that it is 1
-				alternative.AddUtilityTerm(1, destinationParcel.ShadowPriceForEmployment);
+                if (Global.Configuration.ShouldUseShadowPricing)
+                {
+                    alternative.AddUtilityTerm(1, destinationParcel.ShadowPriceForEmployment);
+                }
 
 			}
 		}
