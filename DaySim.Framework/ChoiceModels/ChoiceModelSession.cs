63cbc901c249e9d8066dd2ac035e9f7aa5e2eb5c
Peter Andrews <Peter.Andrews@rsginc.com>
2016-08-26 15:22:13 -0400

    Fix bug since I did not realize this was using Singleton rather than allocating new objects

--- DaySim.Framework/ChoiceModels/ChoiceModelSession.cs
+++ DaySim.Framework/ChoiceModels/ChoiceModelSession.cs
@@ -8,17 +8,39 @@
 using DaySim.Framework.Core;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace DaySim.Framework.ChoiceModels
 {
     public class ChoiceModelSession
     {
+        //could use a ConcurrentDictionary with GetOrAdd method but then the object is possible 
+        //created and initialized multiple times depending on threading
+        //See https://blogs.endjin.com/2015/10/using-lazy-and-concurrentdictionary-to-ensure-a-thread-safe-run-once-lazy-loaded-collection/
+        private readonly Dictionary<Type, IChoiceModel> choiceModelObjectsDictionary = new Dictionary<Type, IChoiceModel>();
+
         public TChoiceModel Get<TChoiceModel>() where TChoiceModel : IChoiceModel
         {
-            Type type = Global.Configuration.getAssignableObjectType(typeof(TChoiceModel));
-            TChoiceModel model = (TChoiceModel)Activator.CreateInstance(type);
-            model.RunInitialize();
-            return model;
-        }
-    }
-}
+            Type requestedType = typeof(TChoiceModel);
+            IChoiceModel choiceModelObject;
+            if (!choiceModelObjectsDictionary.TryGetValue(requestedType, out choiceModelObject))
+            {
+                lock (choiceModelObjectsDictionary)
+                {
+                    //after acquiring lock check that object still needs to be created
+                    if(!choiceModelObjectsDictionary.TryGetValue(requestedType, out choiceModelObject))
+                    {
+                        //create the Singleton of type (or derived type) of TChoiceModel
+                        Type possiblyCustomizedType = Global.Configuration.getAssignableObjectType(requestedType);
+                        choiceModelObject = (IChoiceModel)Activator.CreateInstance(possiblyCustomizedType);
+
+                        choiceModelObject.RunInitialize();
+                        //add to dictionary so we have quick retrieval next time.
+                        choiceModelObjectsDictionary.Add(requestedType, choiceModelObject);
+                    } //end if still not in dictionary after getting lock
+                }   //end lock
+            }   //end if not in dictionary
+            return (TChoiceModel) choiceModelObject;
+        }   //end Get<TChoiceModel>
+    }   //end class
+}   //end namespace
