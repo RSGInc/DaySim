26b51c9cb8b5e3db9581f82d95d4daaf47c87266
Stefan Coe <coestefan@gmail.com>
2017-03-06 16:05:11 -0800

    Changing PSRC transit constants to only impact local bus path type.

--- DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_OtherHomeBasedTourModeModel.cs
+++ DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_OtherHomeBasedTourModeModel.cs
@@ -4,10 +4,10 @@ using DaySim.Framework.DomainModels.Wrappers;
 
 namespace DaySim.ChoiceModels.Default.Models {
     class PSRC_OtherHomeBasedTourModeModel : OtherHomeBasedTourModeModel {
-        protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, ITourWrapper tour, int mode) {
+        protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, ITourWrapper tour, int pathType, int mode) {
             //Global.PrintFile.WriteLine("Default PSRC_WorkTourModeModel.RegionSpecificCustomizations2 called");
 
-            if (mode == Global.Settings.Modes.Transit) {
+            if (mode == Global.Settings.Modes.Transit && pathType != Global.Settings.PathTypes.LightRail && pathType != Global.Settings.PathTypes.CommuterRail && pathType != Global.Settings.PathTypes.Ferry){
 
                 alternative.AddUtilityTerm(200 + tour.OriginParcel.District, 1);//district specific transit calibration constant
             }
--- DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_WorkTourModeModel.cs
+++ DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_WorkTourModeModel.cs
@@ -4,10 +4,11 @@ using DaySim.Framework.DomainModels.Wrappers;
 
 namespace DaySim.ChoiceModels.Default.Models {
     class PSRC_WorkTourModeModel : WorkTourModeModel {
-        protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, ITourWrapper tour, int mode) {
+        protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, ITourWrapper tour, int pathType, int mode)
+        {
             //Global.PrintFile.WriteLine("Default PSRC_WorkTourModeModel.RegionSpecificCustomizations called");
 
-            if (mode == Global.Settings.Modes.Transit) {
+            if (mode == Global.Settings.Modes.Transit && pathType != Global.Settings.PathTypes.LightRail && pathType != Global.Settings.PathTypes.CommuterRail && pathType != Global.Settings.PathTypes.Ferry){
 
                 alternative.AddUtilityTerm(200 + tour.OriginParcel.District, 1);//district specific transit calibration constant
             }
