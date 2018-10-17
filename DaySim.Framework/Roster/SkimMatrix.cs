8fa503a05eabcd352ee52a6b4266b4f157ddc31b
Peter Andrews <Peter.Andrews@rsginc.com>
2018-10-16 23:23:05 -0400

    Actum Skim Matrices truncated to 32767 #171

--- DaySim.Framework/Roster/SkimMatrix.cs
+++ DaySim.Framework/Roster/SkimMatrix.cs
@@ -5,7 +5,6 @@
 // distributed under a License for its use is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-using DaySim.Framework.Core;
 
 namespace DaySim.Framework.Roster {
   public sealed class SkimMatrix {
@@ -16,9 +15,6 @@ namespace DaySim.Framework.Roster {
 
     public ushort GetValue(int origin, int destination) {
       ushort uValue = _skimMatrix[origin][destination];
-      if ((uValue > 32767) && Global.Configuration.DataType == "Actum") {
-        uValue = _skimMatrix[origin][destination] = 32767;
-      }
       return uValue;
     }
 
