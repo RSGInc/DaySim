0f21da12e3a135592fcd9af2d6e3b72120f3f86c
mabcal <mark.bradley@rsginc.com>
2019-04-03 11:45:48 -0700

    Close #235

---
+++ DaySim.Customizations/DVRPC/ChoiceModels/Default/Models/DVRPC_OtherTourDestinationModel.cs
@@ -0,0 +1,36 @@
+﻿using DaySim.Framework.ChoiceModels;
+using DaySim.Framework.Core;
+using DaySim.Framework.DomainModels.Wrappers;
+using DaySim.Framework.Roster;
+
+namespace DaySim.ChoiceModels.Default.Models {
+  internal class DVRPC_OtherTourDestinationModel : OtherTourDestinationModel {
+
+    protected override void RegionSpecificOtherTourDistrictCoefficients(ChoiceProbabilityCalculator.Alternative alternative, ITourWrapper _tour, IParcelWrapper destinationParcel) {
+
+
+      //add any region-specific new terms in region-specific class, using coefficient numbers 114-120, or other unused variable #
+      //Global.PrintFile.WriteLine("JAX_OtherTourDestinationModel.RegionSpecificOtherTourDistrictCoefficients called");
+
+      int origState = _tour.OriginParcel.District;
+      int destState = destinationParcel.District;
+
+      int bridgeFromNJ = (origState == 34 && destState > 0 && destState != 34).ToFlag();
+      int bridgeToNJ = (destState == 34 && origState > 0 && origState != 34).ToFlag();
+      int crossriver = bridgeFromNJ + bridgeToNJ;
+
+      if (_tour.DestinationPurpose == Global.Settings.Purposes.Escort) {
+        alternative.AddUtilityTerm(114, crossriver);
+      } else if (_tour.DestinationPurpose == Global.Settings.Purposes.PersonalBusiness || _tour.DestinationPurpose == Global.Settings.Purposes.Medical) {
+        alternative.AddUtilityTerm(115, crossriver);
+      } else if (_tour.DestinationPurpose == Global.Settings.Purposes.Shopping) {
+        alternative.AddUtilityTerm(116, crossriver);
+      } else if (_tour.DestinationPurpose == Global.Settings.Purposes.Meal) {
+        alternative.AddUtilityTerm(117, crossriver);
+      } else if (_tour.DestinationPurpose == Global.Settings.Purposes.Social || _tour.DestinationPurpose == Global.Settings.Purposes.Recreation) {
+        alternative.AddUtilityTerm(118, crossriver);
+      }
+
+    }
+  }
+}
---
+++ DaySim.Customizations/DVRPC/ChoiceModels/Default/Models/DVRPC_WorkLocationModel.cs
@@ -0,0 +1,21 @@
+﻿using DaySim.Framework.ChoiceModels;
+using DaySim.Framework.Core;
+using DaySim.Framework.DomainModels.Wrappers;
+using DaySim.Framework.Roster;
+
+
+namespace DaySim.ChoiceModels.Default.Models {
+  internal class DVRPC_WorkLocationModel : WorkLocationModel {
+    protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, IPersonWrapper _person, IParcelWrapper destinationParcel) {
+
+      int origState = _person.Household.ResidenceParcel.District;
+      int destState = destinationParcel.District;
+
+      int bridgeFromNJ = (origState == 34 && destState > 0 && destState != 34).ToFlag();
+      int bridgeToNJ   = (destState == 34 && origState > 0 && origState != 34).ToFlag();
+
+      alternative.AddUtilityTerm(91, bridgeFromNJ);
+      alternative.AddUtilityTerm(92, bridgeToNJ);
+    }
+  }
+}
