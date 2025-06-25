89885ac5f4612cf4cadd39ce6ab8723c259343b9
goreaditya <agore@wisc.edu>
2025-06-25 15:13:44 -0400

    Merged in customization from Stefan's develop branch, specific to PSRC. Issue #322

---
+++ DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_AutoOwnershipModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DaySim.Framework.ChoiceModels;
+using DaySim.Framework.DomainModels.Wrappers;
+using DaySim.Framework.Core;
+
+namespace DaySim.ChoiceModels.Default.Models {
+  internal class PSRC_AutoOwnershipModel : AutoOwnershipModel {
+    protected override void RegionSpecificCustomizations(ChoiceProbabilityCalculator.Alternative alternative, IHouseholdWrapper household) {
+      bool Has0To25KIncome = household.Income.IsRightExclusiveBetween(0, 25000);
+      bool Has50To100KIncome = household.Income.IsRightExclusiveBetween(50000, 100000);
+      bool Has100To150KIncome = household.Income.IsRightExclusiveBetween(100000, 150000);
+      bool Has150KPlusIncome = household.Income >= 150000;
+
+      //put on each alternative except 3 (2 vehicles), using the alternative number to number coefficient
+      if (alternative.Id != 3) {
+        alternative.AddUtilityTerm(100 + alternative.Id, Has0To25KIncome.ToFlag());
+        alternative.AddUtilityTerm(110 + alternative.Id, Has50To100KIncome.ToFlag());
+        alternative.AddUtilityTerm(120 + alternative.Id, Has100To150KIncome.ToFlag());
+        alternative.AddUtilityTerm(130 + alternative.Id, Has150KPlusIncome.ToFlag());
+        alternative.AddUtilityTerm(140 + alternative.Id, Math.Log(1 + household.ResidenceParcel.HouseholdDensity1()));
+        alternative.AddUtilityTerm(150 + alternative.Id, Math.Log(1 + household.ResidenceParcel.EmploymentTotalBuffer1));
+      }
+    }
+  }
+}
--- DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_WorkLocationModel.cs
+++ DaySim.Customizations/PSRC/ChoiceModels/Default/Models/PSRC_WorkLocationModel.cs
@@ -12,11 +12,24 @@ namespace DaySim.ChoiceModels.Default.Models
         {
             int homedist = _person.Household.ResidenceParcel.District;
             int zonedist = destinationParcel.District;
+            bool Has0To25KIncome = _person.Household.Income.IsRightExclusiveBetween(0, 25000);
+            bool Has50To100KIncome = _person.Household.Income.IsRightExclusiveBetween(50000, 100000);
+            bool Has100To150KIncome = _person.Household.Income.IsRightExclusiveBetween(100000, 150000);
+            bool Has150KPlusIncome = _person.Household.Income >= 150000;
 
 
-            //add any region-specific new terms in region-specific class, using coefficient numbers 91-97, 99,100 or other unused variable #
-            //Global.PrintFile.WriteLine("Default PSRC_WorkLocationModel.RegionSpecificCustomizations called");
-            int homeSKitWorkTRP = homedist == 11 && (zonedist == 8 || zonedist == 10 || zonedist == 7) ? 1 : 0;
+            double distanceFromOrigin = _person.Household.ResidenceParcel.DistanceFromOrigin(destinationParcel, 1);
+            double distanceLog = Math.Log(1 + distanceFromOrigin);
+
+            alternative.AddUtilityTerm(110, Has0To25KIncome.ToFlag() * distanceLog);
+            alternative.AddUtilityTerm(111, Has50To100KIncome.ToFlag() * distanceLog);
+            alternative.AddUtilityTerm(112, Has100To150KIncome.ToFlag() * distanceLog);
+            alternative.AddUtilityTerm(113, Has150KPlusIncome.ToFlag() * distanceLog);
+
+
+      //add any region-specific new terms in region-specific class, using coefficient numbers 91-97, 99,100 or other unused variable #
+      //Global.PrintFile.WriteLine("Default PSRC_WorkLocationModel.RegionSpecificCustomizations called");
+      int homeSKitWorkTRP = homedist == 11 && (zonedist == 8 || zonedist == 10 || zonedist == 7) ? 1 : 0;
             int homeKitWorkTRP = homedist == 9 && (zonedist == 8 || zonedist == 10 || zonedist == 7) ? 1 : 0;
             int homeEastWorkCBD = homedist == 6 && zonedist == 4 ? 1 : 0;
             int homeKitWorkCBD = (homedist == 9 || homedist == 11) && (zonedist == 4) ? 1 : 0;
@@ -40,7 +53,7 @@ namespace DaySim.ChoiceModels.Default.Models
 
             //pierce county specific constants
 
-            double distanceFromOrigin = _person.Household.ResidenceParcel.DistanceFromOrigin(destinationParcel, 1);
+            //double distanceFromOrigin = _person.Household.ResidenceParcel.DistanceFromOrigin(destinationParcel, 1);
 
             double distance1 = Math.Min(distanceFromOrigin, .35);
             double distance2 = Math.Max(0, Math.Min(distanceFromOrigin - .35, 1 - .35));
